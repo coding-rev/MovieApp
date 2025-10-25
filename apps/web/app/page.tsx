85bcacef430d332ecd6c232075fa92cbc62aae41
Codingrev <emmowu10@gmail.com>
2025-10-25 18:05:29 +0200

    ref: code refactoring

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,7 @@ import { useMovies } from "@/hooks/Queries/useMovies";
 import { Bell, ChevronDown, Play, ListFilter, Funnel, ChevronLeft, ChevronRight } from "lucide-react";
 import ContentWrapper from "@/components/common/ContentWrapper";
 import MovieListVisualization from '@/components/movies/MovieListVisualization';
+import Header from "@/components/common/Header";
 
 export default function Page() {
   const { listMovies, getGenres } = useMovies();
@@ -19,39 +20,8 @@ export default function Page() {
   return (
     <ContentWrapper content={
       <main className="w-full flex flex-col gap-3 text-white">
-      <header className="w-full flex items-center justify-between py-4">
-        <span className="xl min-w-[220px]"><b className="text-4xl">Movies</b>app</span>
-
-        <div className="w-full flex justify-center">
-          <section className="flex items-center justify-between gap-4 h-12 w-[300px] border border-gray-500 bg-black text-white/80 rounded-full px-6">
-            <Link href="#" className="hover:text-white transition text-sm">Movies</Link>
-            <Link href="#" className="hover:text-white transition text-sm">Series</Link>
-            <Link href="#" className="hover:text-white transition text-sm">Originals</Link>
-          </section>
-        </div>
-
-        <section className="flex items-center gap-3 min-w-[220px] justify-end">
-          {/* Notifications */}
-          <button className="relative size-12 rounded-full backdrop-blur-sm bg-white/10 border border-white/20 flex items-center justify-center hover:bg-white/20 transition">
-            <Bell className="size-4"/>
-            <span className="absolute top-1 right-1 size-3 rounded-full bg-red-500 shadow-sm flex items-center justify-center">3</span>
-          </button>
-
-          {/* User Profile Picture */}
-          <div className="size-12 rounded-full overflow-hidden overflow-hidden">
-            <img src="https://avatars.githubusercontent.com/u/66572915?v=4" alt="User avatar" className="w-full h-full object-cover"/>
-          </div>
-
-          {/* Active User */}
-          <div className="flex items-center gap-2">
-            <span className="flex flex-col">
-              <p className="text-sm">Emmanuel</p>
-              <small className="text-xs">Premium</small>
-            </span>
-            {/* <ChevronDown className="size-4"/> */}
-          </div>
-        </section>
-      </header>
+      
+      <Header/>
 
       <section className="w-full flex gap-6">
         <div className="h-[400px] min-w-[500px] rounded-2xl relative overflow-hidden">
